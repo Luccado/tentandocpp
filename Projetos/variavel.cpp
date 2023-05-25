41c226e48e0e5f3b908a38c8f163f513736611af
luccado <luccadioliveira@gmail.com>
2023-05-25 00:32:11 -0400

    Variáveis

---
+++ Projetos/variavel.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int main() {
+
+    /*int x; //declaration
+    x = 5;
+    */
+
+    /*int x = 5;
+    int y = 6;
+    int sum = x+ y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    */
+
+    //integer / inteiro
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    std::cout << days << '\n';
+
+    //double -> número incluindo decimal
+    double price = 10.99;
+    double gpa = 2.5;
+    double temp = 25.1;
+
+    std::cout << price << '\n';
+
+    //single character -> caractere único
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    std::cout << initial << '\n';
+
+    // boolean (true or false) -> booleando verdadeiro ou falso 0 false 1 true
+    bool student = true;
+    bool power = false;
+    bool forSale = true;
+
+    std::cout << student << '\n';
+
+    //String (objeto que representa texto)
+    std::string name = "Lucca";
+    std::string day = "Sexta";
+    std::string food = "Strogonoff";
+    std::string address = "lolol";
+
+    std::cout << "hello " << name << '\n';
+    std::cout << "you are " << age << " years old" << '\n';
+
+    return 0;
+}
