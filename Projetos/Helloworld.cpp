0a6a88dbb1f9de8c16c578b202286a7b516a42b4
luccado <luccadioliveira@gmail.com>
2023-05-25 00:08:09 -0400

    hello world

---
+++ Projetos/Helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    //Isso é um comentário
+    /*
+    Comentário multi-line
+    */
+
+    std::cout << "Eu gosto de pizza!" << '\n';
+    std::cout << "e bem bom!" << '\n';
+    return 0;
+}
